7f86994055e425450106f2214e3ccb0ac5cdb9ae
loredanKon <adrianloredan@yahoo.com>
2020-05-16 18:20:51 +0200

    Cambios que pasan los tests locales

--- src/kata4/snake.py
+++ src/kata4/snake.py
@@ -1,4 +1,5 @@
-import pygame, sys, time, random
+import pygame, sys, time
+from random import randint
 from pygame.locals import *
 
 #pygame.init()
@@ -16,12 +17,16 @@ class Snake():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 self.direction = "LEFT"
+                self.change = self.direction
             if event.key == pygame.K_RIGHT:
                 self.direction = "RIGHT"
-            if event.key == pygame.K_TOP:
-                self.direction = "TOP"
+                self.change = self.direction
+            if event.key == pygame.K_UP:
+                self.direction = "UP"
+                self.change = self.direction
             if event.key == pygame.K_DOWN:
                 self.direction = "DOWN"
+                self.change = self.direction
         
     # Controla el cambio de  las direcciones
     # Orientaciones
@@ -29,23 +34,18 @@ class Snake():
     # Horizontal    -> Movimientos [UP - DOWN]
     # Incremento del movimiento 
     def changeDirection(self):
-        #
-        #
-        #
-        #
-        #
-        #
-        #
-        #
-
-        #
-        #
-        #
-        #
-        #
-        #
-        #
-        #
+        if self.change == "RIGHT":
+            self.direction = "RIGHT"
+            self.position[0] += 10
+        elif self.change == "LEFT":
+            self.direction = "LEFT"
+            self.position[0] -= 10
+        elif self.change == "UP":
+            self.direction = "UP"
+            self.position[1] -= 10
+        elif self.change == "DOWN":
+            self.direction = "DOWN"
+            self.position[1] += 10
 
         self.body.insert(0, list(self.position))
 
@@ -59,38 +59,32 @@ class Game():
 
     # función de salida
     def exit(self, event, pygame):
-        #
+        self.run = False
         #
     
-    # Posición aleatorio entre el ranto [0,49] * 10  
+    # Posición aleatorio entre el rango [0,49] * 10  
     def food_spawn(self):
-        self.food_pos = 0
+        self.food_pos = (randint(0, 49) * 10, randint(0, 49) * 10)
 
     # Si colisionas con una fruta, sumas 1
     # Sino decrementas en 1 el body del snake
     def eat(self, snake):
-        #
-        #    
-        #    
-        #
-        #  
+        if snake.position == self.food_pos:
+            self.score += 1
+        else:
+            snake.body = snake.body[:-1]
 
     # Mensajes de salida cuando el snake muere
     # Posición snake[0] >= 500 ó snake[0] <= 0                  -> Muere
     # Posición snake[1] >= 500 ó snake[1] <= 0                  -> Muere
     # Posición del snake choca con sigo mismo menos la cabeza   -> Muere 
     def dead(self, snake):
-        #
-        #
-        #
-        
-        #
-        #
-        #
-        
-        #
-        #
-        #
+        if snake.position[0] >= 500 or snake.position[0] <= 0:
+            self.run = False
+        elif snake.position[1] >= 500 or snake.position[1] <= 0:
+            self.run = False
+        else:
+            pass
         
             
 # Entry Point
